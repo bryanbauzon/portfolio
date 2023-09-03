0d3553255dcf20e21e03d0ec9a8320220f38f1fb
Max Bryan Bauzon <mrbryanbauzon@gmail.com>
2023-09-03 14:20:03 +0800

    code clean up and installation of tailwindcss

--- app/components/button.js
+++
@@ -1,8 +0,0 @@
-export default function Button() {
-  return (
-    <>
-      <Button>test</Button>
-    </>
-  );
-}
-ß
--- app/page.js
+++ app/page.js
@@ -1,25 +1,12 @@
-import Navbar from "./components/nav";
-import Footer from "./components/footer";
-import Home from "./components/home";
-import About from "./components/about";
-import Services from "./components/services";
-import Contact from "./components/contact";
+
 import Image from "next/image";
 export default async function Page() {
 
   return (
     <>
-    
-     <div>
-        <Navbar />
-        <Home />
-      
-
-        <About />
-        <Services />
-        <Contact />
-        <Footer />
-      </div>  
+   <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
     </>
   );
 }
