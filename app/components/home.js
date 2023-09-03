a9b6e69b42b9908e23067c11b83fd2a88be8b398
bryanbauzon <mrbryanbauzon@gmail.com>
2023-09-03 08:29:04 +0800

    mobile responsiveness

--- app/components/home.js
+++ app/components/home.js
@@ -9,9 +9,11 @@ export default function Home() {
         <h4 className="role">Developer | Photographer | Film maker</h4>
         <h3 className="description">
           A Software Engineer by profession with 4+ years work experience based in
-          the Philippines.Also, a passionate Photographer and Film maker.
+          the Philippines.
+          <br/>
+          Also, a passionate Photographer and Film maker.
         </h3>
-        <h5 id="inprogress">Dev in Progress</h5>
+        <h5 id="inprogress">In Progress</h5>
         {/* <SocialLinks/> */}
       </div>
     </section>
