1464ad4724955ac9915df09fb43302235a39f813
Max Bryan Bauzon <mrbryanbauzon@gmail.com>
2023-09-05 19:48:31 +0800

    temporary removed other links

--- app/components/contact.js
+++
@@ -1,8 +0,0 @@
-export default function Contact() {
-    return (
-        <section className="contactSection" id="contact">
-        Contact
-      </section>
-    );
-  }
-  
--- app/components/home.js
+++ app/components/home.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import SocialLinks from "./social_links";
 
 
@@ -5,18 +6,15 @@ export default function Home() {
   return (
     <section className="homeSection" id="home">
       <div>
-        <h1 className="name">MAX BRYAN BAUZON </h1> 
-        <h4 className="role">Developer | Photographer | Film maker</h4>
+         <h1 className="name">MAX BRYAN BAUZON </h1> 
+        <h4 className="role">Developer - Photographer - Filmmaker</h4>
         <h5 id="inprogress">In Progress</h5>
         <h3 className="description">
-          A Software Engineer by profession with 4+ years work experience based in
+          A <Link href="https://www.linkedin.com/in/bryanbauzon/" className="links" target="_blank">Software Engineer</Link> by profession with 4+ years work experience based in
           the Philippines.
           <br/>
-          Also, a passionate Photographer and Film maker.
+          Also, a passionate <Link href="https://www.facebook.com/brynbzn.films"  className="links">Photographer</Link> and <Link  href="https://www.youtube.com/channel/UCHKE9izs5Z7N9yLWQDFVAlg"  className="links" target="_blank">Filmmaker.</Link>
         </h3>
-        <SocialLinks/> 
-       
-       
       </div>
     </section>
   );
