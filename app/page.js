22c492313c14a2eecfdfe797bcbd60435e11811e
Max Bryan Bauzon <mrbryanbauzon@gmail.com>
2023-09-14 14:32:05 +0800

    fix date

--- app/page.js
+++ app/page.js
@@ -8,13 +8,14 @@ import Projects from "./_components/projects";
 import Experience from "./_components/experience";
 import Achievements from "./_components/achievements";
 export default async function Page() {
+  
   let date = new Date();
   let month = date.getMonth() + 1;
   let day = date.getDate();
 
   let birtday = "";
 
-  if (month == 9 && day == 14) {
+  if (month == 10 && day == 8) {
     birtday = (
       <div className="text-bold text-[5rem] defaultFont">
         AND TODAY IS MY <span className="text-theme">BIRTHDAY</span>!🥳🎉🎊
