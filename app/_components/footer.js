dadc463defd944ab0e81d370548f4fddf2bf32e2
Max Bryan Bauzon <mrbryanbauzon@gmail.com>
2023-10-03 11:36:41 +0800

    Added Hire me button

--- app/_components/footer.js
+++ app/_components/footer.js
@@ -1,6 +1,14 @@
 import Link from "next/link";
 export default function Footer(props) {
   let year = new Date().getFullYear();
+
+  function redirectToEmail() {
+    window.location.href = `mailto:mrbryanbauzon@gmail.com?subject=${
+      props.isVideo
+        ? "<Videographer/Video Editor>"
+        : "<Software Developer Opportunity>"
+    }`;
+  }
   return (
     <footer className="h-[25vh] dark:bg-darkMode flex">
       <div className="flex lg:p-4 md:p-4 p-2 w-full justify-center items-center">
@@ -9,14 +17,24 @@ export default function Footer(props) {
             <div className=" dark:text-white text-darkMode  subFont cursor-default">
               bryanbauzon © {year}
             </div>
-            {props.birthday != ""?<></>:<h2 className=" dark:text-white text-darkMode  subFont text-center">
-              {!props.pathname == '/videos'?<><Link
-                href={"#home"}
-                className="p-2 cursor-n-resize dark:text-white text-darkMode  footerLink hover:underline ml-2 subFont"
-              >
-                Back to Top
-              </Link></>:<></>}
-            </h2>}
+            {props.birthday != "" ? (
+              <></>
+            ) : (
+              <h2 className=" dark:text-white text-darkMode  subFont text-center">
+                {!props.pathname == "/videos" ? (
+                  <>
+                    <Link
+                      href={"#home"}
+                      className="p-2 cursor-n-resize dark:text-white text-darkMode  footerLink hover:underline ml-2 subFont"
+                    >
+                      Back to Top
+                    </Link>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </h2>
+            )}
           </div>
           <div className="grid grid-cols-1 lg:grid-cols-4 gap-1 md:grid-cols-2  ">
             <Link
@@ -50,6 +68,18 @@ export default function Footer(props) {
             </Link>
           </div>
         </div>
+          <Link
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              redirectToEmail();
+            }}
+            className="animate-pulse fixed bottom-10 right-8 bg-theme w-20 h-10  drop-shadow-lg flex justify-center items-center text-darkMode dark:text-white text-small"
+            href={"javascript:void(0);"}
+          >
+            {" "}
+            Hire Me
+          </Link>
       </div>
     </footer>
   );
