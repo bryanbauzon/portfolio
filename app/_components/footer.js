e97bf35d8668f892b21121567ed46b67bd220e70
Max Bryan Bauzon <mrbryanbauzon@gmail.com>
2023-10-15 17:26:16 +0800

    some styles

--- app/_components/footer.js
+++ app/_components/footer.js
@@ -4,8 +4,8 @@ export default function Footer() {
     <div>
       <footer className="h-[25vh] dark:bg-darkMode flex">
         <div className="flex lg:p-4 md:p-4 p-2 w-full justify-center items-center">
-          <div className=" dark:text-white text-darkMode subFont cursor-default">
-            Design & Built by {WEBSITE_NAME}
+          <div className=" dark:text-white text-darkMode defaultFont cursor-default">
+            Design & Built by <span className="text-theme">{WEBSITE_NAME}</span>
           </div>
         </div>
       </footer>
