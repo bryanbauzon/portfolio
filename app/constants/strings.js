38c890eabe9d09bda7a016d3895994f7c32ce452
Max Bryan Bauzon <mrbryanbauzon@gmail.com>
2023-11-22 08:03:33 +0800

    ISJAVA = true

--- app/constants/strings.js
+++ app/constants/strings.js
@@ -61,7 +61,7 @@ export const EXPLORE = "Explore.";
 export const RESUME = "Resume.";
 export const DETAILS = "Details"
 
-export const ISJAVA = false;
+export const ISJAVA = true;
 export const RESUME_DIR_SALESFORCE = "/docs/Max Bryan C. Bauzon - Salesforce Developer.pdf"
 export const RESUME_DIR_JAVA = "/docs/Max Bryan C. Bauzon.pdf"
 
